69717dcbbda1002dd71904016bd8e8d6fe344602
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2022-06-29 00:18:58 -0300

    melhorando codigo missing Number

--- missingNumber.py
+++ missingNumber.py
@@ -2,8 +2,6 @@ n = int(input())
 numbers = input().split()
 newn = []
 numberList = []
-soma1 = 0
-soma2 = 0
 total = 0
 
 #tranforma a lista string em int
@@ -13,9 +11,7 @@ for i in numbers:
 for j in range(1,n+1):
     numberList.append(int(j))
 
-soma1 = sum(newn)
-soma2 = sum(numberList)
-total = soma2 - soma1
+total = sum(numberList) - sum(newn)
 
 print(total)
 
